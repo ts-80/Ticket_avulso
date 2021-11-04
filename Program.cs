de4a22d9ea5d1ed76b5f80c307141ea1aeb7b022
Thiago Siloe <thiagosiloe@gmail.com>
2021-11-04 16:03:54 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ticket_avulso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicketsAvulso tk = new TicketsAvulso();
+            Console.Write("INFORME O CODIGO DO CARTÃO:");
+            for (int i = 0; i <= 9; i++)
+            {
+                
+                int pass=int.Parse(Console.ReadLine());
+                tk.IdCartao.Add(pass);
+            }
+            Console.Write("INFORME O CODIGO DO CLIENTE:");
+            for (int i = 0; i <= 3; i++)
+            {
+
+                int pass = int.Parse(Console.ReadLine());
+                tk.IdCliente.Add(pass);
+            }
+            tk.Calc01();
+            tk.Calc02();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TicketsAvulso.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ticket_avulso
+{
+    class TicketsAvulso
+    {
+        public List<long> IdCartao = new List<long> ();
+        public List<long> IdCliente = new List<long> ();
+        List<long> Soma = new List<long>();
+        List<long> Resto = new List<long>();
+        List<long> Peso = new List<long> {2,3,4,5,6,7,8,9,2,3 };
+        List<long> resultado = new List<long>();
+        long SomaCalc02 = 0;
+
+        
+
+        public void Calc01()
+        {
+            for (int i = 0; i <= 0; i++)
+            {
+                IdCliente.AddRange(IdCliente);
+            }
+            IdCliente.Insert(0, IdCliente[2]);
+            IdCliente.Insert(1, IdCliente[4]);
+            for(int i =0; i<= 9; i++)
+            {
+                long soma01 = IdCartao[i];
+                long soma02 = IdCliente[i];
+                long resultado = soma01 + soma02;
+                long div = resultado % 10;
+                Resto.Add(div);
+                Soma.Add(resultado);
+            }
+            Console.WriteLine("SOMA DO NUMERO DO CLIENTE COM O CODIGO DO CARTÃO:\n");
+            foreach (int iten in Soma)
+            {
+                Console.Write(iten);
+            }
+            Console.WriteLine("\n---------------------------------\n");
+            Console.WriteLine("RESTO ENCONTRADO DO PRIMEIRO CALCULO:\n");
+            foreach (int iten in Resto)
+            {
+                Console.Write(iten);
+            }
+        }
+
+        public void Calc02()
+        {
+            Resto.Reverse();
+            for (int i = 0; i <= 9; i++)
+            {
+                
+                long soma01 = Resto[i];
+                long soma02 = Peso[i];
+                SomaCalc02 += (soma01 * soma02);
+            }
+            SomaCalc02 = (SomaCalc02 * 10) % 11;
+            Resto.Reverse();
+            if (SomaCalc02 != 10)
+            {
+                resultado.AddRange(Resto);
+                resultado.Insert(10, SomaCalc02);
+            }
+            else
+            {
+                resultado.AddRange(Resto);
+                resultado.Insert(10, 0);
+            }
+            Console.WriteLine("\n---------------------------------\n");
+            Console.Write("PRIMEIRO DIGITO VERIFICADOR\n");
+            foreach (long itens in resultado)
+            {
+                Console.Write(itens);
+            }
+
+            /*Console.WriteLine("\n---------------------------------\n");
+            foreach (int iten in Resto)
+            {
+                Console.Write(iten);
+            }
+            Console.WriteLine();
+            foreach (int iten in Resto)
+            {
+                Console.Write(iten);
+            }
+            Console.WriteLine("\n**************************\n");
+            foreach (int iten in Peso)
+            {
+                Console.Write(iten);
+            }
+
+            Console.WriteLine("\n**************************\n");
+
+            foreach (int iten in Resto)
+            {
+                Console.Write(iten);
+            }*/
+        }
+
+    }
+
+}
+
+
